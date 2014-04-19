034574b1dbee953e774eb2f738e24a52b892352c
Josh Bode <joshbode@gmail.com>
2014-04-19 12:29:40 +1000

    synchronised with submitted formula to homebrew-science (r-gui)

--- r-app.rb
+++ r-app.rb
@@ -9,33 +9,30 @@ class RApp < Formula
   head 'https://svn.r-project.org/R-packages/trunk/Mac-GUI'
 
   depends_on :xcode
+  depends_on :macos => :snow_leopard
+  depends_on :arch => :x86_64
+
   depends_on 'r'
 
-  if !Hardware.is_64_bit?
-    onoe "Cannot compile on 32-bit architecture."
-  end
   if MacOS.version >= :mountain_lion
     CONFIG = "MLion64"
   elsif MacOS.version >= :lion
     CONFIG = "Lion64"
   elsif MacOS.version >= :snow_leopard
     CONFIG = "SnowLeopard64"
-  else
-    onoe "Requires Mac OS X Snow Leopard (10.6) or later."
   end
 
   def install
-    if !File.exist?("/Library/Frameworks/R.framework")
-      ln_s "#{HOMEBREW_PREFIX}/opt/r/R.framework",
-           "/Library/Frameworks/R.framework"
-    end
-
     # ugly hack to get updateSVN script in build to not fail
     if build.head?
       cp_r cached_download/".svn", buildpath
     end
 
-    xcodebuild "-target", "R", "-configuration", "#{CONFIG}", "SYMROOT=build"
+    xcodebuild "-target", "R",
+               "-configuration", "#{CONFIG}",
+               "SYMROOT=build",
+               "HEADER_SEARCH_PATHS=#{Formula['r'].opt_prefix}/R.framework/Headers",
+               "OTHER_LDFLAGS=-F#{Formula['r'].opt_prefix}"
 
     prefix.install "build/#{CONFIG}/R.app"
   end
